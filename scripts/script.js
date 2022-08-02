c52a854799dc895550b5e9e1dd8d5e0a5bbb1ab4
itsdjodjo <tsoaramaherison7@gmail.com>
2022-08-02 14:24:59 +0300

    ajout de l'animation fade in

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,19 @@
-// popup
-const open = document.getElementById('open');
-const close = document.getElementById('close');
-const popupContainer = document.getElementById('popup-container');
+// fadeIn
+window.addEventListener('scroll', reveal)
 
-open.addEventListener('click', () => {
-    popupContainer.classList.add('active');
-});
-close.addEventListener('click', () => {
-    popupContainer.classList.remove('active');
-});
+function reveal() {
+    var faders = document.querySelectorAll('.fade-in');
+
+    for(let i = 0; i < faders.length; i++) {
+        let windowHeight = window.innerHeight;
+        let revealTop = faders[i].getBoundingClientRect().top;
+        let revealPoint = 150;
+
+        if(revealTop < windowHeight - revealPoint) {
+            faders[i].classList.add('show-content');
+        }
+        else {
+            faders[i].classList.remove('show-content');
+        }
+    }
+};
